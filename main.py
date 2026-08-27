119c66cab2b554a1cb43373ce504665cb6d74d69
dixit <dixitbuild@gmail.com>
2026-08-27 23:18:39 +0530

    Added Post api

--- main.py
+++ main.py
@@ -7,16 +7,23 @@ app = FastAPI()
 def greet():
     return "welcome to Fast API"
 
-product = [
+products = [
     Products(id=1, product='phone', discription='keypad', price=1200, quantity=3),
     Products(id=2, product='phone', discription='smart', price=12000, quantity=31),
     Products(id=3, product='ipad', discription='from apple', price=12000, quantity=41),
 ]
 @app.get('/products')
 def get_products():
-    return product
+    return products
 
 #dynamic routing
 @app.get('/product/{id}')
 def get_product(id: int):
-    return product[id-1]
+    return products[id-1]
+
+
+#post method
+@app.post('/product')
+def get_product(product: Products):
+    products.append(product)
+    return product
